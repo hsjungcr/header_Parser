7f6de6e5b312ec625820fc06f39b4ed69a4ed70b
root <root@955307901.acs.edcc.edu>
2017-04-05 09:29:13 -0700

    ipaddr, lang and sw

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 var express = require('express')
-var moment = require('fs')
-var path = require('path')
-
 var app = express()
 var port = process.env.PORT||8000
+var bodyParser = require('body-parser')
+
 
 app.listen(port,() => console.log('Listening on port: ' + port))
-app.get('/', (req, res) => {
-	let fileName = path.join(__dirname,'index.html')
-	res.sendFile(fileName, (err) => {
-		if(err) {
-			console.log(err)
-			res.status(err.status).end()
-		}
-		else {
-			console.log('Sent: ', filename)
-		}
-	}
-}
+
+app.get('/' , (req, res)=>{
+	var lang = req.headers["accept-language"].split(",")[0]
+	var software = req.headers["user-agent"].match(/\((.+?)\)/)[1]
+	var out = {"ipaddress":req.ip,"language":lang,"software":software}
+	res.send(out)
+})
+
+app.get('/',(req,res)=>{
+	res.send(req.body)
+})
+
+//app.get('/', (req, res) => {
+//	let fileName = path.join(__dirname,'index.html')
+//	res.sendFile(fileName, (err) => {
+//		if(err) {
+//			console.log(err)
+//			res.status(err.status).end()
+//		}
+//		else {
+//			console.log('Sent: ', filename)
+//		}
+//	}
+//}
